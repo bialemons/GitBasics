c6d5aaf4967cba43eec5d7a95063178ea5cbfaee
bialemons <bialemos73@gmail.com>
2016-09-29 04:38:36 -0300

    teste

--- GitBasics/ViewController.swift
+++ GitBasics/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
